9f60c5287d3772d60ab68565707e8faaaee08b93
SuRyeon-Lee <bunnyatgym@gmail.com>
2022-09-25 02:17:25 +0900

    :construction: 그림판: Line Width

--- app.js
+++ app.js
@@ -1,31 +1,39 @@
+const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
-ctx.lineWidth = 2;
+ctx.lineWidth = lineWidth.value;
 
-const colors = [
-    '#55efc4',
-    '#81ecec',
-    '#74b9ff',
-    '#a29bfe',
-    '#00b894',
-    '#00cec9',
-    '#0984e3',
-    '#6c5ce7',
-    '#ffeaa7',
-    '#fab1a0',
-    '#ff7675',
-    '#fd79a8'
-]
+//moveTo는 그림을 그리지 않고 그리기 시작할 위치만 예약한다.
+//따라서 마우스가 움직일 때마다 moveTo로 초점을 따라가면 그리기 시작할때 그 위치부터 그릴 수 있다.
+let isPainting;
+function onMove(event){
+    if(isPainting){
+        ctx.lineTo(event.offsetX, event.offsetY);
+        ctx.stroke();
+        return;
+    }
+    ctx.moveTo(event.offsetX,event.offsetY); //초점 따라다니다가
+}
+function onMouseDown(event){
+    isPainting = true;
+}
+function cancelPainting(){
+    isPainting = false;
+    ctx.beginPath();
+}
+function onLineWidthChange(event){
+    ctx.lineWidth = event.target.value
+}
 
-const onClick = (e) => {
-    ctx.beginPath()
-    ctx.moveTo(0,0);
-    const color = colors[Math.floor(Math.random() * colors.length)];
-    ctx.strokeStyle = color;
-    ctx.lineTo(e.offsetX, e.offsetY);
-    ctx.stroke();
-};
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", onMouseDown);
+canvas.addEventListener("mouseup", cancelPainting);
 
-canvas.addEventListener('mousemove',onClick);
+//캔버스를 떠나서 마우스 떼도 돌아오면 드로잉 상태인 것을 해결하기 위해
+//1. document에 이벤트를 걸어주거나
+// document.addEventListener("mouseup", cancelPainting);
+//2. 아님 아래와 같이 mouseleave이벤트를 걸어줄 수 있음
+canvas.addEventListener("mouseleave", cancelPainting);
+lineWidth.addEventListener("change", onLineWidthChange)
